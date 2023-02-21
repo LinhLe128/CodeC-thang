9d05abc6a873c9aab532143f3382b03e71528c82
LinhLe128 <90230859+LinhLe128@users.noreply.github.com>
2023-02-21 07:48:47 +0700

    Initial commit

---
+++ test/test/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace test
+{
+    class Franction
+    {
+        private int ts, ms;
+        public int TS
+        {
+            get { return ts; }
+            set { ts = value; }
+        }
+        public int MS {
+            get { return ms; }
+            set { ms = (value == 0) ? 1 : value; }
+        }
+        public Franction()
+        {
+            ts = 0;
+            ms = 1;
+        }
+        public Franction(int tts, int tms)
+        {
+            ts = tts;
+            ms = tms;
+        }
+        public void display()
+        {
+            Console.WriteLine("{0}/{1}", ts, ms);
+        }
+        public static Franction operator +(Franction ta, Franction tb)
+        {
+            Franction kq = new Franction();
+            kq.ts = ta.ts * tb.ms + ta.ms * tb.ms;
+            kq.ms = ta.ms + tb.ms;
+            return kq;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Franction ps1 = new Franction();
+            Franction ps2 = new Franction(2, 3);
+            Franction ps3 = new Franction();
+            
+            ps1.display();
+            ps2.display();
+
+            ps3 = ps1 + ps2;
+            ps3.display();
+        }
+    }
+}
